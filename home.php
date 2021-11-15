3750f8d4e820a391926cbd5fbcd06b13b5dbc064
TOUIL Mohammed <taouil_moh@upf.ac.ma>
2021-11-15 11:12:26 +0100

    commit_7:new commit in master to merge branches later

--- home.php
+++ home.php
@@ -2,5 +2,6 @@
     echo("Hello word 2 ");
     echo("Changes after creating first commit in new Branch");
     echo("Changes N2 after creating first commit in new Branch");
+    echo("Changes in master after creating third commit in new Branch 2");
 
 ?>
